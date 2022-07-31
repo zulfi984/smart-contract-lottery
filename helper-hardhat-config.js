0f6b511f6a05e3d87ec36f044210b294b7a4e470
zulfi984 <zulfizee1@gmail.com>
2022-07-31 16:57:23 +0530

    altered heper-hardhat

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -28,13 +28,13 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+//const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+//const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
 
 module.exports = {
     networkConfig,
     developmentChains,
     VERIFICATION_BLOCK_CONFIRMATIONS,
-    frontEndContractsFile,
-    frontEndAbiFile,
+    // frontEndContractsFile,
+    // frontEndAbiFile,
 }
